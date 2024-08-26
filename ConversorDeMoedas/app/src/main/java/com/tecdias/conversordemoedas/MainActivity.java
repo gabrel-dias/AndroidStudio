4f2feaf34475869f8131580c78ba940613ae955e
Gabriel Dias <gabrielmdais557@gmail.com>
2024-08-25 22:35:07 -0300

    valor convertido de dólares para reais

--- ConversorDeMoedas/app/src/main/java/com/tecdias/conversordemoedas/MainActivity.java
+++ ConversorDeMoedas/app/src/main/java/com/tecdias/conversordemoedas/MainActivity.java
@@ -1,6 +1,10 @@
 package com.tecdias.conversordemoedas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // relacionando os elementos da Activity principal aos da GUI no XML (essa relação não é automática)
+        Button btnConverterReal = findViewById(R.id.btnConverter);
+        EditText txtQtdDolar = findViewById(R.id.txtQuantiaDolares);
+        TextView txtQtdReal = findViewById(R.id.txtResultado);
+
+        // criando um listener para usar o botão
+        btnConverterReal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // usando a cotação de: 26 de ago., 00:46 UTC · Fontes
+                double dolar = 5.49d * Double.parseDouble(txtQtdDolar.getText().toString());
+                String valorFormatado = String.format("%.2f",dolar);
+                txtQtdReal.setText("R$" + valorFormatado);
+            }
         });
     }
 }
