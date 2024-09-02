336866c14b699d2aaca1406ceee72c331b34b4e4
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-02 01:53:03 -0300

    até agora tudo certo mas tem espaço para melhorias

--- FrasesNerds/app/build.gradle.kts
+++ FrasesNerds/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "2.8"
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- FrasesNerds/app/src/main/java/com/tecdias/frasesnerds/MainActivity.java
+++ FrasesNerds/app/src/main/java/com/tecdias/frasesnerds/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         gerarFrase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // criando um HashMap com 10 frases
+                // criando um HashMap com 10 frases nerds
                 Map<Integer, String> mapFrasesGeradas = new HashMap() {
                     {
                         put(0, "\"Seu futuro não está escrito, o de ninguém está. Você pode fazer o que quiser fazer.\"\n(De Volta Para o Futuro)");
@@ -59,13 +59,14 @@ public class MainActivity extends AppCompatActivity {
                         put(2, "\"Que a Força esteja com você!\"\n(Star Wars)");
                         put(3, "\"Com grandes poderes, vem grandes responsabilidades.\"\n(Homem-Aranha)");
                         put(4, "\"O inverno está chegando.\"\n(Game of Thrones)");
-                        put(5, "\"Não são as nossas qualidades, mas sim as nossas escolhas que revelam quem nós somos.\"\n (Harry Potter)");
+                        put(5, "\"Não são as nossas qualidades, mas sim as nossas escolhas que revelam quem nós somos.\"\n(Harry Potter)");
                         put(6, "\"Existe sempre uma solução.\"\n(Doctor Who)");
                         put(7, "\"Não deixe os trouxas te colocarem para baixo.\"\n(Harry Potter)");
                         put(8, "\"It's bigger on the inside!\"\n(Qualquer um ao entrar na TARDIS pela primeira vez em Doctor Who)");
                         put(9, "\"One ring to rule them all.\"\n(Senhor dos Anéis)");
                     }
                 };
+
                 // usando Random para criar uma chave aleatória e poder resgatar um valor contido no map
                 Random valor = new Random();
                 // deixando a frase visível
