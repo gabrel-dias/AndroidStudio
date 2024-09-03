e1b1f0f202f31cbad008ae57a04e3aee557144ad
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-02 22:05:49 -0300

    melhorias fodas e está pronto para a entrega do trabalho

--- FrasesNerds/app/build.gradle.kts
+++ FrasesNerds/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "3.6"
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
