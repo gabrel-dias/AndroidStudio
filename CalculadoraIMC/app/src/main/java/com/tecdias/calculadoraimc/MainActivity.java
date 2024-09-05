d0e21becd32cd5e0ff56f785b40de7f3f2fe80c0
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-05 10:40:18 -0300

    protótipo calculador de imc

--- CalculadoraIMC/app/src/main/java/com/tecdias/calculadoraimc/MainActivity.java
+++ CalculadoraIMC/app/src/main/java/com/tecdias/calculadoraimc/MainActivity.java
@@ -1,12 +1,19 @@
 package com.tecdias.calculadoraimc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button botaoCalculo = findViewById(R.id.btnCalcular);
+        TextView txtAltura = findViewById(R.id.txtAltura);
+        TextView txtPeso = findViewById(R.id.txtPeso);
+        TextView resultadoIMC = findViewById(R.id.txtResultado);
+
+        botaoCalculo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double altura = Double.parseDouble(txtAltura.getText().toString());
+                double peso = Double.parseDouble(txtPeso.getText().toString());
+                double imc = peso / (Math.pow(altura, 2));
+
+                DecimalFormatSymbols virgula = new DecimalFormatSymbols(Locale.getDefault());
+                DecimalFormat formatador = new DecimalFormat("#,##0.00", virgula);
+
+                resultadoIMC.setText(formatador.format(imc));
+            }
         });
     }
 }
