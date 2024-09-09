2706a8c8cb8d7e542da9dbdb47c0d1259af50dfb
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-09 01:46:40 -0300

    aprendendo radios mais ou menos

--- TestesUdemy/app/src/main/java/com/tecdias/testesudemy/MainActivity.java
+++ TestesUdemy/app/src/main/java/com/tecdias/testesudemy/MainActivity.java
@@ -1,12 +1,17 @@
 package com.tecdias.testesudemy;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraint), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -26,8 +31,28 @@ public class MainActivity extends AppCompatActivity {
 
         CheckBox homem = findViewById(R.id.homem);
         CheckBox mulher = findViewById(R.id.mulher);
-        TextView texto = findViewById(R.id.txtRadio);
+        TextView texto = findViewById(R.id.txtCheckbox);
+        RadioGroup radioGroup = findViewById(R.id.radioCores);
+        RadioButton azul = findViewById(R.id.azul);
+        RadioButton vermelho = findViewById(R.id.vermelho);
+        RadioButton verde = findViewById(R.id.verde);
+        Button btnmudaCor = findViewById(R.id.buttonMudaCor);
+        ConstraintLayout layout = findViewById(R.id.constraint);
 
+
+        // testando os radioButtons
+        btnmudaCor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (azul.isChecked()) {
+                    layout.setBackgroundColor(Color.BLUE);
+                } else if (vermelho.isChecked()) {
+                    layout.setBackgroundColor(Color.RED);
+                } else layout.setBackgroundColor(Color.GREEN);
+            }
+        });
+
+        // testando as checkboxes
         homem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
