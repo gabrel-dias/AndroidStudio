dac50ab2d20e9a646db477aaafa55deebdf8063c
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-26 22:48:22 -0300

    finalizar verificação de campos vazios e implementar lógica do cálculo

--- Combustivel/app/src/main/java/com/tecdias/combustivel/MainActivity.java
+++ Combustivel/app/src/main/java/com/tecdias/combustivel/MainActivity.java
@@ -1,6 +1,10 @@
 package com.tecdias.combustivel;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,58 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        EditText editAlcool = findViewById(R.id.editAlcool);
+        EditText editGasolina = findViewById(R.id.editGasolina);
+        Button botaoCalculo = findViewById(R.id.botaoCalcular);
+        TextView textResultado = findViewById(R.id.textResultado);
+
+        double alcool = 0;
+        double gasolina = 0;
+
+        String alcoolText = editAlcool.getText().toString();
+        String gasolinaText = editGasolina.getText().toString();
+
+        while (alcoolText.isEmpty()) {
+            textResultado.setVisibility(View.VISIBLE);
+            textResultado.setText("Digite o preço do álcool!");
+        }
+
+        while (gasolinaText.isEmpty()) {
+            textResultado.setVisibility(View.VISIBLE);
+            textResultado.setText("Digite o preço da gasolina!");
+        }
+        botaoCalculo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
+
+/*
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+
+	    System.out.println("Digite o preço do litro do alcool:");
+	    double alcool = sc.nextDouble();
+	    System.out.println("Digite o preço do litro da gasolina:");
+	    double gasolina = sc.nextDouble();
+
+	    double divisao = alcool/gasolina;
+	    System.out.printf("%.2f\n", divisao);
+
+	    if(alcool/gasolina < 0.7){
+	        System.out.println("Vale a pena usar alcool!");
+	    } else
+	    System.out.println("Vale a pena usar gasolina!");
+
+	}
+}
+
+https://meubolsoemdia.com.br/calculadora-alcool-gasolina
+ */
