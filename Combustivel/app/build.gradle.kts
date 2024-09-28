6079a920a8ae07f4714e74540a4ab382fc1551ad
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-28 17:09:17 -0300

    lógica finalizada e código comentado

--- Combustivel/app/build.gradle.kts
+++ Combustivel/app/build.gradle.kts
@@ -10,8 +10,7 @@ android {
         applicationId = "com.tecdias.combustivel"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- Combustivel/app/src/main/java/com/tecdias/combustivel/MainActivity.java
+++ Combustivel/app/src/main/java/com/tecdias/combustivel/MainActivity.java
@@ -30,61 +30,64 @@ public class MainActivity extends AppCompatActivity {
         TextView textAviso = findViewById(R.id.textAviso);
         TextView textResultado = findViewById(R.id.textResultado);
         Button botaoCalculo = findViewById(R.id.botaoCalcular);
+        textAviso.setVisibility(View.INVISIBLE);
+        textResultado.setVisibility(View.INVISIBLE);
 
         botaoCalculo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textAviso.setVisibility(View.INVISIBLE);
-                textResultado.setVisibility(View.INVISIBLE);
+
+                // atribuindo os valores dos campos em Strings
                 String stringAlcool = editAlcool.getText().toString();
                 String stringGasolina = editGasolina.getText().toString();
                 double alcool = 0;
                 double gasolina = 0;
                 double resultado;
 
-                /* verificações dos campos individualmente
-                 if (stringGasolina.isEmpty() && stringAlcool.isEmpty()) {
-                    textAviso.setText("Digite os valores de álcool e gasolina!");
-                }
-                // verifica se o campo da gasolina está vazio e o álcool não
-                else if (stringGasolina.isEmpty() && !stringAlcool.isEmpty()) {
-                    textAviso.setText("Digite o preço da gasolina!");
-                    // verifica se o campo da gasolina está vazio e o álcool não
-                } else if (!stringGasolina.isEmpty() && stringAlcool.isEmpty()) {
-                    textAviso.setText("Digite o preço do álcool!");
-                }*/
                 try {
+                    // converte os valores das strings em double, o que irá gerar uma exceção se
+                    // os campos estiverem vazios
                     alcool = Double.parseDouble(stringAlcool);
                     gasolina = Double.parseDouble(stringGasolina);
                 } catch (NumberFormatException e) {
-                    if (stringAlcool.isEmpty()) {
+                    // caso haja a exceção NumberFormatException, são feitas verificações para
+                    // descobrir qual dos campos está vazio e então gerar uma mensagem orientando
+                    // o usuário
+                    if (stringGasolina.isEmpty() && stringAlcool.isEmpty()) {
+                        // os dois campos estão vazios
                         textAviso.setVisibility(View.VISIBLE);
-                        textAviso.setText("Digite o valor no campo do álcool");
+                        textAviso.setText("Digite os dois valores");
+                    } else if (stringAlcool.isEmpty()) {
+                        // apenas um dos campos está vazio
+                        textAviso.setVisibility(View.VISIBLE);
+                        textAviso.setText("Digite o valor do álcool");
                     } else if (stringGasolina.isEmpty()) {
                         textAviso.setVisibility(View.VISIBLE);
-                        textAviso.setText("Digite o valor no campo da gasolina");
+                        textAviso.setText("Digite o valor da gasolina");
                     }
                 }
-                textAviso.setVisibility(View.INVISIBLE);
+                // aplica a "regra dos 70%" para descobrir qual combustível é mais vantajoso
                 resultado = alcool / gasolina;
+                System.out.println(resultado);
 
-                if (resultado <= 0.7) {
-                    textResultado.setVisibility(View.VISIBLE);
-                    textResultado.setText("Vale a pena usar alcool!");
-                    if (stringAlcool.isEmpty() || stringGasolina.isEmpty()) {
-                        textResultado.setVisibility(View.INVISIBLE);
-                    }
-                } else {
-                    textResultado.setVisibility(View.VISIBLE);
-                    textResultado.setText("Vale a pena usar gasolina!");
-                    if (stringAlcool.isEmpty() || stringGasolina.isEmpty()) {
-                        textResultado.setVisibility(View.INVISIBLE);
+                textResultado.setVisibility(View.VISIBLE);
+
+                if (stringAlcool.isEmpty() || stringGasolina.isEmpty()) {
+                    // verifica se algum dos campos ficou vazio e esconde o resultado,
+                    // pois o valor da divisão será zero
+                    textResultado.setVisibility(View.INVISIBLE);
+                }
+                if (!stringGasolina.isEmpty() && !stringAlcool.isEmpty()) {
+                    // se nenhum dos campos estiver vazio, a mensagem orientando o usuário irá desaparecer
+                    textAviso.setVisibility(View.INVISIBLE);
+                    if (resultado <= 0.7) {
+                        // define o melhor com base no resultado da divisão entre os dois valores
+                        textResultado.setText("Vale a pena usar álcool!");
+                    } else {
+                        textResultado.setText("Vale a pena usar gasolina!");
                     }
                 }
-
             }
         });
-
-
     }
 }
