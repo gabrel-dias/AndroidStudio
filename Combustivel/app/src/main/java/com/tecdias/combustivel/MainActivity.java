2bb22980725b05cd84913efd551f7b5aa76cf94d
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-28 11:13:32 -0300

    fiquei puto e refiz tudo

--- Combustivel/app/src/main/java/com/tecdias/combustivel/MainActivity.java
+++ Combustivel/app/src/main/java/com/tecdias/combustivel/MainActivity.java
@@ -24,52 +24,49 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         EditText editAlcool = findViewById(R.id.editAlcool);
         EditText editGasolina = findViewById(R.id.editGasolina);
-        Button botaoCalculo = findViewById(R.id.botaoCalcular);
         TextView textAviso = findViewById(R.id.textAviso);
         TextView textResultado = findViewById(R.id.textResultado);
+        Button botaoCalculo = findViewById(R.id.botaoCalcular);
 
         botaoCalculo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // iniciando variáveis que receberão os valores dos campos do usuário
+                String stringAlcool = editAlcool.getText().toString();
+                String stringGasolina = editGasolina.getText().toString();
                 double alcool = 0;
                 double gasolina = 0;
 
-                // verificando se os campos estão vazios com o método isEmpty() da classe String
-                try {
-                    String alcoolText = editAlcool.getText().toString();
-                    String gasolinaText = editGasolina.getText().toString();
-                    if (alcoolText.isEmpty() && !gasolinaText.isEmpty()) {
-                        textAviso.setVisibility(View.VISIBLE);
-                        textAviso.setText("Digite o preço do álcool!");
-                    } else {
-                        alcool = Double.parseDouble(alcoolText);
-                    }
 
-                    if (gasolinaText.isEmpty() && !alcoolText.isEmpty()) {
-                        textAviso.setVisibility(View.VISIBLE);
-                        textAviso.setText("Digite o preço da gasolina!");
-                    } else {
-                        gasolina = Double.parseDouble(gasolinaText);
-                    }
-                } catch (NumberFormatException e) {
-                    textAviso.setText("Digite os dois valores");
-
+                /* verificações dos campos individualmente
+                 if (stringGasolina.isEmpty() && stringAlcool.isEmpty()) {
+                    textAviso.setText("Digite os valores de álcool e gasolina!");
                 }
+                // verifica se o campo da gasolina está vazio e o álcool não
+                else if (stringGasolina.isEmpty() && !stringAlcool.isEmpty()) {
+                    textAviso.setText("Digite o preço da gasolina!");
+                    // verifica se o campo da gasolina está vazio e o álcool não
+                } else if (!stringGasolina.isEmpty() && stringAlcool.isEmpty()) {
+                    textAviso.setText("Digite o preço do álcool!");
+                }*/
+                try {
+                    alcool = Double.parseDouble(stringAlcool);
+                    gasolina = Double.parseDouble(stringGasolina);
+                } catch (NumberFormatException e) {
+                    textAviso.setText("Os dois campos precisam de valores para calcular o combustível ideal");
+                }
+
                 double resultado = alcool / gasolina;
-
-
-                if (resultado < 0.7d) {
-                    textAviso.setVisibility(View.VISIBLE);
-                    textAviso.setText("O resultado é zero\nDigite os dois campos!");
-                } else if (resultado == 0.7d) {
-                    textResultado.setVisibility(View.VISIBLE);
-                    textResultado.setText("Vale a pena usar álcool!");
+                if (resultado < 0.7) {
+                    System.out.println("Vale a pena usar alcool!");
                 } else
-                    textResultado.setText("Vale a pena usar gasolina!");
+                    System.out.println("Vale a pena usar gasolina!");
+
             }
         });
+
+
     }
 }
